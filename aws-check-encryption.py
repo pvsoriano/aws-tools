55a02579ded375a89e904fb1304f7cc64529d21c
Paul Soriano <pvsoriano@me.com>
2019-03-20 16:59:12 -0500

    Cleaned up script
    Cleaned up script to make it read better.

--- aws-check-encryption.py
+++ aws-check-encryption.py
@@ -1,29 +1,16 @@
 import boto3
 from pprint import pprint
 
-# Setup session using RAP credentials
-session = boto3.Session(profile_name='ddpa-rap-poc')
+# Setup session using local credentials
+session = boto3.Session(profile_name='')
 s3 = session.resource('s3')
 client = session.client('s3')
 
-# Determining encrypting by checking object encryption within each bucket
-#bucket = s3.Bucket('s3-ue2-agent-jar-delta')
+# Determine encryption by checking encryption at the bucket level
+Bucket = s3.buckets.all()
 
-#RAPBucket = s3.buckets.all()
-
-#for bucket in RAPBucket:
-#	for obj in bucket.objects.all():
-#		key = s3.Object(bucket.name, obj.key)
-#		if key.server_side_encryption == None:
-#			print(obj.key, key.server_side_encryption)
-
-
-# Determining encryption by checking encryption at the bucket level
-RAPBucket = s3.buckets.all()
-
-#response = client.get_bucket_encryption(Bucket='s3-ue2-agent-jar-delta')
-#print(response)
-for x in RAPBucket:
+# Iterate over the response to print out the bucket name and if it's encrypted or not
+for x in Bucket:
 	y = x.name
 	response = client.get_bucket_encryption(Bucket=y)
 	#pprint(response)
